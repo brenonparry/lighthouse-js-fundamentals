6dde77cb5563a9e3fe755c07c0e779b4c65202b2
Brenon Parry <brenonparry@gmail.com>
2021-11-03 19:13:39 +0000

    the isEven code

---
+++ even.js
@@ -0,0 +1,11 @@
+// const isEven  = function (num) {
+//   return num % 2 === 0;
+// };
+// const num = isEven(7);
+// console.log(num);
+
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+};
+console.log(isEven(746));
