6799b9645d31e53e54912411b5d6cfceb1a2fb7a
Brenon Parry <brenonparry@gmail.com>
2021-11-04 23:34:42 +0000

    Messing around with For Loops in an Array

---
+++ packing-list.js
@@ -0,0 +1,32 @@
+/* --------------------- FOR LOOPS ---------------------- */
+
+
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+// console.log("Kitchen stuff to pack:");
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+
+
+
+// for (let i = 0; i < packingList.length; i++) {
+//   let stuff
+//   return packingList[i];
+// }
+  
+// console.log("Kitchen stuff to pack: " + packingList[i]);
+
+
+// function vacationTime(Mystuff) {
+//   for (let i = 0; i < packingList.length; i++) {
+//   let Mystuff = packingList[i];
+//   return Mystuff;
+//   }
+// }
+// console.log("Kitchen stuff to pack: " + Mystuff);
+
+
+/* ---------------------------- WHILE LOOPS ------------------------- */
+
